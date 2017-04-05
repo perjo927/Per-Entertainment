56f4687391b7eaa0677abb95c893636f7590a843
perjo927 <perjo927@student.liu.se>
2017-04-05 21:13:14 +0200

    feat(init): add routes

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ var app = express()
 // http://localhost:3000/images/0.jpg
 app.use(express.static('public'))
 
+// TODO: handlers
 app.get('/', (req, res) => res.send('Welcome to Per Entertainment!'))
+app.get('/game/:id', (req, res) => res.send(req.params))
+app.get('/game/:id/play', (req, res) => res.send({
+  "outcome": [0,1,2],
+  "winType": "noWin",
+  "bonus": false
+}))
 
-app.listen(3000, () => console.log('App listening on port 3000'))
+/**
+ * Serve resources
+ * Calculate outcome, 3 integers: [0, 0, 0]
+ * Type of win: (No Win, Small Win, Big Win)
+   ** Two equal integers = Small Win. Three equal integers = Big Win.
+ * Randomly (in addition to outcome) return if bonus feature should be triggered or not.
+ * 
+ */
+
+app.listen(3000, () => console.log('Entertainment served on port 3000'))
