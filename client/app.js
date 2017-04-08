0bd3865aa96328923a150d05be2fa0ae03e250f0
perjo927 <perjo927@student.liu.se>
2017-04-08 17:44:50 +0200

    feat(client-server): add spin button

--- client/app.js
+++ client/app.js
@@ -11,6 +11,7 @@ const options = {
 };
 
 // GET resources
+// TODO: handlers
 fetch('http://localhost:3000/game/1', options)
     .then((response) => {
         return response.json();
@@ -22,12 +23,13 @@ fetch('http://localhost:3000/game/1', options)
             name: json.name
         }
     }).then((resources) => {
-
         let h1 = document.querySelector('h1');
         h1.textContent = resources.name;
 
         let resourceContainer = document.querySelector('.resources');
+        let button = document.querySelector('button');
 
+        // Symbols
         for (let resource of resources.symbols) {
             fetch(`${host}${resource}`, options)
                 .then(function (response) {
@@ -40,6 +42,7 @@ fetch('http://localhost:3000/game/1', options)
                     resourceContainer.appendChild(img);
                 });
         }
+        // Button
         fetch(`${host}${resources.button}`, options)
             .then(function (response) {
                 return response.blob();
@@ -48,18 +51,27 @@ fetch('http://localhost:3000/game/1', options)
                 const objectURL = URL.createObjectURL(imgBlob);
                 let img = document.createElement("img");
                 img.src = objectURL;
-                resourceContainer.appendChild(img);
+
+                button.appendChild(img);
+                button.onclick = () => {
+                    fetch('http://localhost:3000/game/1/newround', options)
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then(function (json) {
+                            const { bonus, outcome, winType } = json;
+                            if (bonus) {
+                                console.log("BONUS")
+                            }
+                            console.log(outcome);
+                            console.log(winType);                            
+                        });
+                }
             });
     });
 
 
-fetch('http://localhost:3000/game/1/newround', options)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (json) {
-        console.log(json)
-    });
+
 
 
 
