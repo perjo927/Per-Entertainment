7d7e3a441fd6d8d7e3d2b3fdf8535ada01273a19
perjo927 <perjo927@student.liu.se>
2017-04-09 09:43:21 +0200

    feat(client-server): refactor client implementation

--- client/app.js
+++ client/app.js
@@ -11,6 +11,8 @@ const options = {
     cache: 'default'
 };
 
+let symbols = [];
+
 const getGameResources = async () => {
     let response = await fetch(`${host}game/${gameId}`, options);
     let json = await response.json();
@@ -33,61 +35,69 @@ const getImgBlob = async (resource) => {
 }
 
 const getSymbols = async (resources, container) => {
-    let imgBlob, objectURL, img, response;
     let objectURLs = [];
 
     for (let resource of resources.symbols) {
         const imgBlob = await getImgBlob(resource);
-
-        // objectURLs.push(URL.createObjectURL(imgBlob));
-
-        objectURL = URL.createObjectURL(imgBlob);
-        img = document.createElement("img");
-        img.src = objectURL;
-        container.appendChild(img);
+        objectURLs.push(URL.createObjectURL(imgBlob));
     }
+    return objectURLs;
 }
 
 const handleButtonClick = async () => {
     const gameRoundData = await getNewGameRound();
-
     const { bonus, outcome, winType } = gameRoundData;
 
-    if (bonus) {
-        console.log("FREE SPIN")
-    }
-    console.log(outcome);
+    // TODO
     console.log(winType);
+
+    let objectURL, img;
+    const container = document.querySelector('.slot-grid');
+    // Clear grid, if anything in the grid        
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // TODO setSymbols(container)
+    for (let symbol of outcome) {        
+        let url = this.symbols[symbol];
+        console.log(symbol, url)
+        img = document.createElement("img");
+        img.src = url;
+        container.appendChild(img);
+    }
+
+    if (bonus) {
+        // TODO
+        console.log("FREE SPIN")
+        await handleButtonClick();
+    }
 }
 
 const getButton = async (resources, button) => {
-    let imgBlob, objectURL, img, response;
-    imgBlob = await getImgBlob(resources.button);
-
-    objectURL = URL.createObjectURL(imgBlob);
-    img = document.createElement("img");
+    const imgBlob = await getImgBlob(resources.button);
+    const objectURL = URL.createObjectURL(imgBlob);
+    let img = document.createElement("img");
     img.src = objectURL;
     button.appendChild(img);
-
     button.onclick = handleButtonClick;
 }
 
-const initGameResources = async () => {
-    let response, json;
+const setTitle = (resources, element) => {
+    element.textContent = resources.name;
+}
 
+const initGameResources = async () => {
+    let page = document.querySelector.bind(document);
     const resources = await getGameResources();
 
-    let h1 = document.querySelector('h1');
-    h1.textContent = resources.name;
-    let resourceContainer = document.querySelector('.resources');
-    let button = document.querySelector('button');
-
-    await getSymbols(resources, resourceContainer);
-    await getButton(resources, button);
+    setTitle(resources, page('h1'));
+    this.symbols = await getSymbols(resources, page('.resources'));
+    await getButton(resources, page('button'));
 }
+
 initGameResources();
-// GET resources
-// TODO: handlers
+
 
 
 
